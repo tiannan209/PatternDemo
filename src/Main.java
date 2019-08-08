3576a3e456c5280cc8451102e60f36ca0e1b63d6
tiannan209 <tiannan209@hotmail.com>
2019-08-08 16:06:06 +1000

    Factory pattern updated

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,12 @@ public class Main {
         System.out.println("String from x is " + x.s);
         System.out.println("String from y is " + y.s);
         System.out.println("String from z is " + z.s);
+
+        //prac demo for factory pattern
+        System.out.println("Factory pattern demo");
+        Computer pc = ComputerFactory.getComputer("pc","2 GB","500 GB","2.4 GHz");
+        Computer server = ComputerFactory.getComputer("server","16 GB","1 TB","2.9 GHz");
+        System.out.println("Factory PC Config::"+pc);
+        System.out.println("Factory Server Config::"+server);
     }
 }
